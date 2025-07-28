80d40b6fbc079a384eac15a8b9be08e60e9766a5
Iván Chavero <ichavero@chavero.com.mx>
2025-07-28 17:42:07 -0600

    Removerd soft tab configuration

--- nvim/lua/global/options.lua
+++ nvim/lua/global/options.lua
@@ -4,7 +4,6 @@ vim.o.clipboard = "unnamedplus" -- Sync clipboard between neoviem and the OS
 vim.o.autoindent = true         -- Autoindent
 vim.o.shiftwidth = 4            -- Number of spaces inserted for each indentation
 vim.o.tabstop = 4               -- Insert n spaces for a tab
-vim.o.softtabstop = 4           -- 
 vim.o.expandtab = true          -- Convert tabs to spaces
 vim.opt.termguicolors = true    -- set termguicolors to enable highlight groups
 vim.o.wrap = false              -- display lines as one long line
@@ -14,6 +13,5 @@ vim.o.fileencoding = 'utf-8'    -- the encoding written to a file
 vim.o.signcolumn = 'yes'        -- Keep signcolumn on by default
 vim.o.list = true               -- Show invisible characters
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' } -- What to show for this characters
-
 
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- separate vim plugins from neovim in case vim still in use
